97f708e7f4e524457f1996f3ec9e4d0dd51193dd
ISHABACHARYA <ishabachary@gmail.com>
2022-05-26 20:09:14 +0545

    german encoded sysadmin translation using case and string literals

---
+++ lib/german_sysadmin.ex
@@ -0,0 +1,18 @@
+defmodule Username do
+  def sanitize(''), do: ''
+
+  def sanitize([head | tail]) do
+    sanitized =
+      case head do
+        ?ä -> 'ae'
+        ?ö -> 'oe'
+        ?ü -> 'ue'
+        ?ß -> 'ss'
+        x when x >= ?a and x <= ?z -> [x]
+        ?_ -> '_'
+        _ -> ''
+      end
+
+    sanitized ++ sanitize(tail)
+  end
+end
